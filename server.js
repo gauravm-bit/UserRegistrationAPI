61cf392987cae6fcb749a9de685ae4a7be36a240
gauravm-bit <gauravwrk01@gmail.com>
2020-05-14 19:06:52 +0530

    ADDED:database connection to the server

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const dbConfig = require('./config/database.config.js')
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(bodyParser.json())
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
 
 app.get('/',(req,res) => {
     res.json({"message":"Welcome to User Registration API"})
