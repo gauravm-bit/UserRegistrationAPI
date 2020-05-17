b79cfc26b4e97410bea37a2346122fa5c6f001f6
gauravm-bit <gauravwrk01@gmail.com>
2020-05-17 22:55:06 +0530

    REFACTORED:model file and used callback

--- app/models/user.js
+++ app/models/user.js
@@ -23,4 +23,57 @@ const userSchema = mongoose.Schema({
 })
 
 let userModel = mongoose.model('users',userSchema);
+
+class Usermodel 
+{
+    findOne(req)
+    {
+        return new Promise((resolve,reject) => 
+        {
+            User.findOne(req)
+            .then(data => 
+                {
+                  resolve(data);
+                })
+            .catch(err=>
+                {
+                    console.log(err)
+                    reject(err)
+                })
+        })
+    }
+
+    register(req,callback)
+    {
+        const user = new User({
+             username = req.body.username,
+             email = req.body.email,
+             password = req.body.password,
+
+        })
+        user.save((err,result)=>
+        {
+            if(err)
+                callback(err)
+            else
+            {
+                let response = {
+                    email:result.email,
+                    sucess:true,
+                }
+                callback(null,response)
+            }
+        })
+    }
+    login(req,callback)
+    {
+        let response =
+        {
+            username:req.body.username,
+            email:req.body.email,
+            message:'Success'
+        }
+        callback(null,response)
+    }
+}
 module.exports = userModel;
